759e2fd0e98bcdf62eb408b8d6df7bd2042db896
wushanghui <wushanghui@aliyun.com>
2020-07-29 18:02:49 +0800

    code

---
+++ spring/spring-test/src/main/java/top/wushanghui/profile/DataSource.java
@@ -0,0 +1,45 @@
+package top.wushanghui.profile;
+
+/**
+ * @author wsh
+ * @date 2020/7/29 12:23
+ */
+public class DataSource {
+
+    private String url;
+    private String username;
+    private String password;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "DataSource{" +
+                "url='" + url + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
